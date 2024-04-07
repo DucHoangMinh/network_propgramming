8da70d60c3685169c8f3901377548d0413d4ae14
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-04-07 11:27:19 +0700

    add poll server client

--- chuong3_cac_vi_du_client_server/poll_select/poll_server.c
+++ chuong3_cac_vi_du_client_server/poll_select/poll_server.c
@@ -6,8 +6,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <poll.h>
+#include <time.h>
 
 #define MAX_FDS 2048
+#define MAX_NAME 256
+
+char client_names[MAX_FDS][MAX_NAME];
 
 int main() {
     // Tao socket cho ket noi
@@ -21,7 +25,7 @@ int main() {
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(8000);
+    addr.sin_port = htons(9000);
 
     // Gan socket voi cau truc dia chi
     if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) {
@@ -50,27 +54,28 @@ int main() {
         if (ret == -1) {
             break;
         }
-        if (ret == 0) {
-            printf("Timed out!!!\n");
-            continue;
-        }
 
         for (int i = 0; i < nfds; i++) {
             if (fds[i].revents & POLLIN) {
                 if (fds[i].fd == listener) {
                     // Co ket noi moi
-                    int client = accept(listener, NULL, NULL);
-                    if (client >= MAX_FDS) {
-                        // Vuot qua so luong toi da
-                        close(client);
-                    } else {
-                        // Them vao mang tham do su kien
-                        fds[nfds].fd = client;
-                        fds[nfds].events = POLLIN;
-                        nfds++;
+                    // Co ket noi moi
+                int client = accept(listener, NULL, NULL);
+                if (client >= MAX_FDS) {
+                    // Vuot qua so luong toi da
+                    close(client);
+                } else {
+                    // Them vao mang tham do su kien
+                    fds[nfds].fd = client;
+                    fds[nfds].events = POLLIN;
+                    nfds++;
 
-                        printf("New client connected: %d\n", client);
-                    }
+                    printf("New client connected: %d\n", client);
+
+                    // Gui yeu cau nhap ten va id cho client moi
+                    char *message = "Please enter your id and name in the format 'client_id: client_name'\n";
+                    send(client, message, strlen(message), 0);
+                }
                 } else {
                     // Co du lieu tu client truyen den
                     int client = fds[i].fd;
@@ -80,7 +85,26 @@ int main() {
                     } else {
                         // Xu ly du lieu nhan duoc
                         buf[ret] = 0;
-                        printf("Received from %d: %s\n", client, buf);
+                        if (strncmp(buf, "client_id: ", 11) == 0) {
+                            strncpy(client_names[client], buf + 11, MAX_NAME);
+                            printf("Client %d is now known as %s\n", client, client_names[client]);
+                        } else {
+                            printf("Received from %d: %s\n", client, buf);
+
+                            // Gui tin nhan den cac client khac
+                            for (int j = 1; j < nfds; j++) {
+                                if (fds[j].fd != client) {
+                                    char message[512];
+                                    time_t now = time(NULL);
+                                    struct tm *t = localtime(&now);
+                                    sprintf(message, "%04d/%02d/%02d %02d:%02d:%02d %s: %s",
+                                            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+                                            t->tm_hour, t->tm_min, t->tm_sec,
+                                            client_names[client], buf);
+                                    send(fds[j].fd, message, strlen(message), 0);
+                                }
+                            }
+                        }
                     }
                 }
             }
